24ae9d1f3c90138401087dc4a2bea33b5b968309
Arka Mukherjee <85149698+Rendro1@users.noreply.github.com>
2023-12-26 21:08:06 +0530

    changes after db connection

---
+++ src/index.js
@@ -0,0 +1,58 @@
+// require('dotenv').config()
+import dotenv from "dotenv"
+import connectDB from "./db";
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// first approach to to connect db in index file if we do not want to use db folder we can connect from here also with the help of below code and we also write this code on db folder in a file and import here to run and connect the database so it's the approach number 2.
+
+/*
+import express from "express";
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", () => {
+      console.log("Err: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console, log(`application is listening on ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error: ", error);
+    throw error;
+  }
+})();
+
+*/
