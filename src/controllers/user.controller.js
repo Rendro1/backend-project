90fdf244fd67a55a6ba58f68498c7770b89f991b
Arka Mukherjee <85149698+Rendro1@users.noreply.github.com>
2024-02-24 20:10:07 +0530

    getWatchHistory endpoint is completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -394,6 +394,58 @@ const getUserChannelProfile = asyncHandler(async(req,res)=>{
     )
 
 })
+
+const getWatchHistory = asyncHandler(async(req,res)=>{
+  const user = await User.aggregate([
+    {
+      $match:{
+        _id: new mongoose.Types.objectId(req.user._id)
+      }
+    },
+    {
+      $lookup:{
+        from:"Video",
+        localField:"watchHistory",
+        foreignField:"_id",
+        as:"watchHistory",
+        pipeline:[
+          {
+            $lookup:{
+              from:"User",
+              localField:"owner",
+              foreignField:"_id",
+              as:"owner",
+              pipeline:[
+                {
+                  $project:{
+                    fullname:1,
+                    username:1,
+                    avatar:1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields:{
+              owner:{
+                $first:"$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+
+  res
+  .status(200)
+  .json(new ApiResponse(200,user[0].watchHistory,"Watch History Fetched Successfully!!"))
+})
+
+
+
+
 export {
   registerUser,
   loginUser,
@@ -404,5 +456,6 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
-  getUserChannelProfile
+  getUserChannelProfile,
+  getWatchHistory
 };
